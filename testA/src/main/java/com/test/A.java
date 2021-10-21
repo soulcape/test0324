5175afe431a68b6e1cb78cb55708cb4f8b41a59a
jinlingjian <jinlingjian@hikvision.com.cn>
2021-10-21 15:19:24 +0800

    add ccccc

--- testA/src/main/java/com/test/A.java
+++ testA/src/main/java/com/test/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("AAAAAA");
         System.out.println("BBBBBB");
+        System.out.println("CCCCCC");
     }
 }
